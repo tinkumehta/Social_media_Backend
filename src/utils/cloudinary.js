2142e493cb71f570899bcc13a979353bf184b18b
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-02-27 12:45:10 +0530

    fix all register controllers

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,6 +18,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             })
         // file has been uploaded successfull
         console.log("file is uploaded on cloundinary", response.url);
+        fs.unlinkSync(localFilePath)
          return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
