7a56c19e32075a0b84a32580589031ece96b0dfc
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-04-08 17:06:54 +0530

    feat: add playlist routes and controller for managing playlists

--- src/app.js
+++ src/app.js
@@ -21,12 +21,15 @@ import userRouter from './routes/user.routes.js'
 import healthCheckRouter from "./routes/healthcheck.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import videoRouter from "./routes/video.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/health", healthCheckRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/video", videoRouter)
+app.use("/api/v1/playlist", playlistRouter)
+
 
 // http://localhost:8000/api/v1/users/register
 
