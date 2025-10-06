08495b2e4ac6cf1df8d0f2be5bff9a621dfe0d80
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-10-06 13:49:14 +0530

    fix: update package.json scripts and correct DB_NAME in constants.js; clean up index.js and format vercel.json

--- src/controllers/constants.js
+++ src/controllers/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "SocalMedaiGighub"
+export const DB_NAME = "TestBackend"
--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ import { app } from "./app.js"
     })
 
     connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 8000 , () => {
-            console.log(`Server is running at port :${process.env.PORT}`);
+    // .then(() => {
+    //     app.listen(process.env.PORT || 8000 , () => {
+    //         console.log(`Server is running at port :${process.env.PORT}`);
             
-        })
-    })
-    .catch((err) => {
-        console.log("MONGO db connection failed !!! ", err);
+    //     })
+    // })
+    // .catch((err) => {
+    //     console.log("MONGO db connection failed !!! ", err);
         
-    })
+    // })
 
+
+    module.exports = app
 
 
 
